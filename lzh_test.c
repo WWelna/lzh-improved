ccbd4889595c1f0cf4476c332eb017ed42ef2e29
William Welna <Sanguinarious@OccultusTerra.com>
2016-05-04 01:17:36 -0400

    Update + License

--- lzh_test.c
+++ lzh_test.c
@@ -1,3 +1,26 @@
+/*
+* Copyright (C) 2016 William H. Welna All rights reserved.
+*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions are met:
+*     * Redistributions of source code must retain the above copyright
+*       notice, this list of conditions and the following disclaimer.
+*     * Redistributions in binary form must reproduce the above copyright
+*       notice, this list of conditions and the following disclaimer in the
+*       documentation and/or other materials provided with the distribution.
+*
+* THIS SOFTWARE IS PROVIDED BY William H. Welna ''AS IS'' AND ANY
+* EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+* WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+* DISCLAIMED. IN NO EVENT SHALL William H. Welna BE LIABLE FOR ANY
+* DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+* (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+* LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+* ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+* (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+* SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
 /* lzh test program - @Sanguinarious <Sanguinarious@OccultusTerra.com> */
 
 #include <stdio.h>
@@ -83,8 +106,8 @@ int main(int argc, char **argv) {
 	printf("%i Decompressed %i to %i\n", y, x, d.out_written);
 	memset(out, 'X', x);
 	if(memcmp(out, test_buffer, x))
-		printf("Success Bitches\n");
+		printf("Success\n");
 	else
-		printf("Bastard\n");
+		printf("Fail\n");
 	return 0;
 }
